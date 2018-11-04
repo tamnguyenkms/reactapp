887d70df4c6a90a5a82bf66bdb39dcdf97c7d159
Tam Nguyen <hithanhtam@xbot.vn>
2018-11-04 22:23:31 +0700

    todolist

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import "./App.css";
+import TodoList from "./TodoList";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1> Hello, World! </h1>
+      <div>
+        <TodoList />
       </div>
     );
   }
